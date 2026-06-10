0878a4b94455551f5e0db9cc2d11b0deeceef9f0
Eduardo Quiquia <eduardo.quiquia@tecsup.edu.pe>
2026-06-10 17:14:26 -0500

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      DRAGON BALL
-    </h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Dragon Ball</Button>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
